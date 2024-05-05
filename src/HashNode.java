6171bf406d9527810e68af9c24af50b03061238f
tastenalibek <156886419+tastenalibek@users.noreply.github.com>
2024-05-05 20:07:37 +0500

    -

--- src/HashNode.java
+++ src/HashNode.java
@@ -1,20 +1,4 @@
-// HashNode class definition
-class HashNode<K, V> {
-    K key;
-    V value;
-    HashNode<K, V> next;
 
-    public HashNode(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    // Overriding toString for easy debugging
-    @Override
-    public String toString() {
-        return "{" + key + "=" + value + '}';
-    }
-}
 
 
 
